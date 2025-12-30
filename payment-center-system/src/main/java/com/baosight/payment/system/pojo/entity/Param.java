e3dd9adff0bbfc152ae52c403d7e6fa2f21a94b9
ycj <cuijie_yang@163.com>
2025-12-30 15:41:26 +0800

    fix(pay-center) 新增支付接口时，增加参数的格式校验

--- payment-center-system/src/main/java/com/baosight/payment/system/pojo/entity/Param.java
+++ payment-center-system/src/main/java/com/baosight/payment/system/pojo/entity/Param.java
@@ -1,11 +1,18 @@
 package com.baosight.payment.system.pojo.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * 参数结构定义
  */
 @Data
+@NoArgsConstructor
+@JsonIgnoreProperties
 public class Param {
     /**
      * 字段名称
@@ -26,4 +33,18 @@ public class Param {
      * 字段中文名
      */
     private String description;
+
+    // 核心：通过 @JsonCreator 标记构造器，@JsonProperty(required = true) 强制生效
+    @JsonCreator
+    public Param(
+            @JsonProperty(value = "name", required = true) String name, // 强制必填
+            @JsonProperty(value = "sort", required = true) Integer sort,
+            @JsonProperty(value = "required", required = true) Boolean required,
+            @JsonProperty(value = "description", required = true) String description
+    ) {
+        this.name = name;
+        this.sort = sort;
+        this.required = required;
+        this.description = description;
+    }
 }
